47076c26f70df013d6d570e16c944dfb631096d2
Lukete Henrique <luketevl@gmail.com>
2019-09-25 16:24:50 -0300

    Updates

---
+++ precache-manifest.212e4646dd22315f1204349ac8e96027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cab2a20ed147b7717652b9649f59012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5eb5ba0d821c4373e5d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3eff3ca787e82f7e091b",
+    "url": "/static/js/2.861e4154.chunk.js"
+  },
+  {
+    "revision": "c5eb5ba0d821c4373e5d",
+    "url": "/static/js/main.e5c5df16.chunk.js"
+  },
+  {
+    "revision": "ee6c36ee18b04d5487af",
+    "url": "/static/js/runtime-main.83c34cc9.js"
+  }
+]);
